efd442afca20fcd1f99a8914eb85467101d64dd8
Steven Brown <dev@steveb27.com>
2026-07-27 03:52:06 -0400

    add pawn class

--- lab-projects/player-interface/main.py
+++ lab-projects/player-interface/main.py
@@ -1,7 +1,23 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
 
 class Player(ABC):
     def __init__(self):
         self.moves = []
         self.position = (0, 0)
         self.path = [self.position]
+
+    def make_move(self):
+        """Adds a move to a random position"""
+
+    @abstractmethod
+    def level_up(self):
+        """"""
+
+
+class Pawn(Player):
+    def __init__(self):
+        super().__init__()
+
+    def level_up(self):
+        """"""
