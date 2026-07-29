2741bf77d5bb04725a38b402b35c09c209c1a65b
Steven Brown <dev@steveb27.com>
2026-07-29 13:30:43 -0400

    Add player interface lab

--- lab-projects/player-interface/main.py
+++ lab-projects/player-interface/main.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-
+import random
 
 class Player(ABC):
     def __init__(self):
@@ -7,17 +7,33 @@ class Player(ABC):
         self.position = (0, 0)
         self.path = [self.position]
 
-    def make_move(self):
-        """Adds a move to a random position"""
-
     @abstractmethod
     def level_up(self):
-        """"""
+        pass
+
+    def make_move(self):
+        """Adds a move to a random position"""
+        move = random.choice(self.moves)
+        position = tuple(sum(x) for x in zip(move, self.position))
+        self.position = position
+        self.path.append(position)
+        return move
 
 
 class Pawn(Player):
     def __init__(self):
         super().__init__()
+        self.moves = [
+            (0,1),
+            (0,-1),
+            (-1,0),
+            (1,0)
+        ]
 
     def level_up(self):
-        """"""
+        self.moves += [
+            (1,1),
+            (1,-1),
+            (-1,1),
+            (-1,-1)
+        ]
