73328dbef091705c0ce41eb86c0bd8830daea2f5
Steven Brown <dev@steveb27.com>
2026-07-30 19:42:06 -0400

    Debug key-error, add nested dictionary for hash key collision, fix typing error (self typod sef)

--- certification-projects/p4-hash-table/main.py
+++ certification-projects/p4-hash-table/main.py
@@ -6,16 +6,30 @@ class HashTable:
         return sum([ord(char) for char in key])
 
     def add(self, key, value):
-        pass
+        hashkey = self.hash(key)
+        if hashkey not in self.collection:
+            self.collection[hashkey] = dict()
+        self.collection[hashkey][key] = value
 
-    def remove(self, key, value):
-        pass
+    def remove(self, key):
+        hashkey = self.hash(key)
+        if hashkey in self.collection:
+            if key in self.collection[hashkey]:
+                del self.collection[hashkey][key]
+                if len(self.collection[hashkey]) == 0:
+                    del self.collection[hashkey]
 
     def lookup(self, key):
-        pass
+        hashkey = self.hash(key)
+        if hashkey in self.collection.keys() and key in self.collection[hashkey]:
+            return self.collection[hashkey][key]
+        return None
 
 
 key = 'golf'
-hashkey = HashTable().hash(key)
+hashtable = HashTable()
+hashtable.add('rose', 'flower')
+hashkey = hashtable.hash(key)
 
 print(f"{key}: {hashkey}")
+print(hashtable.collection)
